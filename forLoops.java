edc843068b586c2adfeb5beb488994388481c0b6
Your Name <you@example.com>
2025-03-06 16:36:21 +0530

    two forloops completed

---
+++ forLoops.java
@@ -0,0 +1,12 @@
+package loopsConcept;
+
+import org.testng.annotations.Test;
+
+public class forLoops {
+
+	@Test
+	private void nestedforLoops() {
+
+	}
+
+}
--- src/test/java/forLoops.java
+++ src/test/java/forLoops.java
@@ -1,9 +1,29 @@
+import org.testng.annotations.Test;
 
 public class forLoops {
 
-	private void nestedforLoops() {
+	@Test(dependsOnGroups = "enhancedforLoop")
+	private void SingleForloop() {
 
-		//writting from hub
+		int number = 12345;
+
+		String s = String.valueOf(number);
+
+		for (int i = 0; i < s.length(); i++) {
+
+			System.out.println(s.charAt(i));
+		}
+	}
+
+	@Test(groups  = "enhancedforLoop")
+	private void enhancedforLoop() {
+
+		char[] ch = { 'a', 'b', 'c', 'd' };
+
+		for (int j = 0; j < ch.length; j++) {
+
+			System.out.println(ch[j]);
+		}
 
 	}
 
