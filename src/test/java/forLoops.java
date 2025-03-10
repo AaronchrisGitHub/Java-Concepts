db407e36e6e10658c3d02742665df69f64c9dee6
Your Name <you@example.com>
2025-03-11 00:12:37 +0530

    forloopupdated

--- src/test/java/forLoops.java
+++ src/test/java/forLoops.java
@@ -2,7 +2,7 @@ import org.testng.annotations.Test;
 
 public class forLoops {
 
-	@Test(dependsOnGroups = "enhancedforLoop")
+	@Test(dependsOnGroups = "SingleForloop")
 	private void SingleForloop() {
 
 		int number = 12345;
@@ -15,16 +15,43 @@ public class forLoops {
 		}
 	}
 
-	@Test(groups  = "enhancedforLoop")
+	@Test(groups = "enhancedforLoop")
 	private void enhancedforLoop() {
 
 		char[] ch = { 'a', 'b', 'c', 'd' };
 
-		for (int j = 0; j < ch.length; j++) {
+		for (int j = 1; j < ch.length; j++) {
 
 			System.out.println(ch[j]);
 		}
 
+	}
+
+	@Test
+	void LabeledForLoop() {
+
+		int count =0;
+		stop: for (int i = 0; i < 10; i++) {
+
+			for (int j = 0; j < i+1; j++) {
+				
+				count++;
+
+				if (i == 1 && j == 1) {
+					System.out.println("now the i value is" + i + "j value is " + j);
+					System.out.println("count value is "+count);
+					break stop;
+				}
+
+			}
+		}
+
+	}
+
+	@Test
+	void infiniteForLoop() {
+
+		
 	}
 
 }
